f0c42c4ac4d442436a4f0c1d73a73b6c45d35e7c
Toby Mackenzie <public@tobymackenzie.com>
2023-03-28 16:02:20 -0400

    code: remove some unused stuff

--- src/DependencyInjection/TJMSySiteExtension.php
+++ src/DependencyInjection/TJMSySiteExtension.php
@@ -4,7 +4,6 @@ namespace TJM\SySite\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -18,7 +17,6 @@ class TJMSySiteExtension extends Extension{
 	public function load(array $configs, ContainerBuilder $container){
 		$configuration = new Configuration();
 		$config = $this->processConfiguration($configuration, $configs);
-//var_dump($configs);die();
 		$pageWraps = Array(
 			"bare"=> "@TJMSySite/shells/bare.html.twig"
 			,"simple"=> "@TJMSySite/shells/simple.html.twig"
@@ -28,7 +26,5 @@ class TJMSySiteExtension extends Extension{
 			$pageWraps = array_merge($pageWraps, $config["page_wraps"]);
 		}
 		$container->setParameter("tjm_sy_site.wraps", $pageWraps);
-		// $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-		// $loader->load('services.yml');
 	}
 }
